08ca8491caf01c67199bed0e58f24a208334f9a1
YoussefGemy <youssef.gemy00@gmail.com>
2023-12-22 21:18:43 +0200

    Chapter07

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Xml.Linq;
+using static System.Console;
+
+var doc = new XDocument();
+string s1="Hello";
+String s2="World";
+WriteLine($"{s1} {s2}");
---
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+namespace DotNetCoreEverywhere{
+    class Program{
+        static void Main(string[] args){
+            WriteLine("I can run everywhere!");
+        }
+    }
+}
+//dotnet publish -c Release -r osx-x64 
+//dotnet publish -c Release -r win-x64 
