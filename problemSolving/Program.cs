e19f18493cc517e91a43a4aec9d4660ecf56453c
YoussefGemy <youssef.gemy00@gmail.com>
2024-01-06 06:11:39 +0200

    Chapter09

--- problemSolving/Program.cs
+++ problemSolving/Program.cs
@@ -1,25 +1,29 @@
-﻿using System.Runtime.Intrinsics.X86;
-
-string[] bank=["011001","000000","010100","001000"];
-int  r1=0;
-int r2=0;
-int ans = 0;
-for(int i=0;i<bank.Length;i++) {
-    int cnt = 0;
-    foreach (var ch in bank[i]) {
-        if (ch == '1') cnt++;
-    }
-    if (cnt != 0){
-        if (r1 == 0) {
-            r1 = cnt;
-            continue;
+﻿public class Solution {
+    public int MinOperations(int[] nums) {
+        Dictionary<int,int>mp=new();
+        for(int i=0;i<nums.Length;i++){
+            if(mp.ContainsKey(nums[i])){
+                mp[nums[i]]+=1;
+            }
+            else{
+                mp.Add(nums[i],1);
+            }
         }
-        else if (r2 != 0) {
-            r1 = r2;
+        int ans=0;
+        foreach(var v in mp){
+            int u=v.Value;
+            if(u==1){
+                return -1;
+            }
+            else if(u==2){
+                ans+=1;
+            }
+            else{
+                ans+=(int)(u/3);
+                u%=3;
+                if(u!=0)ans+=1;
+            }
         }
-        r2 = cnt;
-        ans += r1 * r2;
+        return ans;
     }
 }
-
-Console.WriteLine(ans);
