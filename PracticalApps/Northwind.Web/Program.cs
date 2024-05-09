727cf0ce5547232af6ab0d186ba94b225b39d82f
YoussefGemy <youssef.gemy00@gmail.com>
2024-05-09 17:07:16 +0300

    web

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,13 +1,28 @@
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+#region Configure the web server host and services.
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
+#endregion
+
+
+
+//this configure to redirect the http to => https
 #region Configure the HTTP pipeline and routes
 if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
 app.UseHttpsRedirection();
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+//app.MapGet("/", () => $"Environment is {app.Environment.EnvironmentName}");
+app.MapGet("/hello", () => $"Environment is {app.Environment.EnvironmentName}");
 #endregion
 
 // Start the web server, host the website, and wait for requests.
--- PracticalApps/Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
+++ PracticalApps/Northwind.Web/obj/Debug/net8.0/Northwind.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea023bd3711ce3988d6b5dc29a2878e6e2a11176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+921d492b98c6abf1a72921776913074d0a63cb0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
